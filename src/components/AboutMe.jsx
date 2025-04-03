e3916e377b739e490055610d8602ee520596f669
NethuDinsara <neth.hettihewa@gmail.com>
2025-04-03 14:56:52 +0530

    Updated AboutMe

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -10,13 +10,13 @@ function AboutMe() {
       onMouseLeave={() => setIsHovered(false)}
       style={{ transform: isHovered ? 'scale(1.03)' : 'scale(1)', transition: 'transform 0.3s ease-in-out' }}
     >
-      <h2>About Me</h2>
+    
       <img src="src/assets/images/dinsara.png" alt="Your Profile" />
       <p>
-        Hello! I'm John Doe, a passionate and creative full-stack developer with a strong foundation in building scalable and user-friendly web applications. My journey in the world of technology began with a fascination for problem-solving and a desire to bring ideas to life through code.
+      Hello! I'm Dinsara Hettihewa, a passionate and creative Software Engineering undergrad focused on building innovative full-stack solutions. My journey in technology is fueled by a desire to create impactful digital experiences.
       </p>
       <p>
-        With a background in Computer Science, I've honed my skills in various technologies, including React, Node.js, and modern CSS frameworks. I'm always eager to learn and explore new tools and techniques to enhance my craft and deliver innovative solutions.
+      Skilled in React, React Native, Spring Boot, and Node.js, with database experience in MySQL and MongoDB. My leadership and teamwork abilities are cultivated through volunteering in Leo and Rotaract. I'm eager to contribute to collaborative and innovative projects.
       </p>
       {isHovered && <p className="hover-message">Thanks for exploring my profile!</p>}
     </div>
